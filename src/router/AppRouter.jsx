cd480952be28491ec66662f6adc5a59d398c1c54
Miguel Angel Caro <miguelangel.caro@iesdonana.org>
2022-11-09 15:53:06 +0100

    cambio algunas cosas de router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,18 @@
-import { Route, Routes } from "react-router-dom"
-import { PortfolioRoutes } from "../portfolio";
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import { BiografiaPage, ContactoPage, PortfolioPage, ProyectosPage} from "../portfolio/index";
 
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path="/*" element={<PortfolioRoutes /> } />
-        </Routes>
+      <Routes>
+        <Route path="portfolio" element={<PortfolioPage />} />
+        <Route path="biografia" element={<BiografiaPage />} />
+        <Route path="proyectos" element={<ProyectosPage />} />
+        <Route path="contacto" element={<ContactoPage />} />
+
+        <Route path="/" element={<Navigate to="/portfolio" />} />
+
+      </Routes>
     </>
   )
 }
