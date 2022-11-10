b1f33d8af08cc7025f86182e14f821c1e2745426
Miguel Angel Caro <miguelangel.caro@iesdonana.org>
2022-11-10 21:27:51 +0100

    cambio de browser a hash para probar

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-portfolio">
+    <HashRouter >
         <PortfolioApp />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
