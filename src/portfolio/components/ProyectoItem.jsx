7e075b71a9551184c0d8158cd8ca87785b664d47
Miguel Angel Caro <miguelangel.caro@iesdonana.org>
2023-04-17 14:57:23 +0200

    feat: tarjetas proyectos con titulo de estado

--- src/portfolio/components/ProyectoItem.jsx
+++ src/portfolio/components/ProyectoItem.jsx
@@ -6,10 +6,28 @@ export const ProyectoItem = () => {
         <>
             {
                 proyectos.map((proyect) => (
-                    <a key={ proyect.title } className="card__link" href={proyect.url} target="_blank">
+                    proyect.finalizado  
+                    ? <a key={ proyect.title } className="card__link" href={proyect.url} target="_blank">
                         <div className="card">
                             <h2>{proyect.title}</h2>
                             <p className="card__title">{proyect.description}</p>
+                            <p className="card__apply">{proyect.language}<i className="fas fa-arrow-right"></i>
+                            </p>
+                        </div>
+                    </a>
+                    : proyect.title == "orientaT" ?
+                    <a key={ proyect.title } className="card__link card_in_process" href={proyect.url} target="_blank">
+                        <div className="card">
+                            <h2>{proyect.title} <small>( en migración )</small></h2>
+                            <p className="card__title card_in_process">{proyect.description}</p>
+                            <p className="card__apply">{proyect.language}<i className="fas fa-arrow-right"></i>
+                            </p>
+                        </div>
+                    </a>
+                    : <a key={ proyect.title } className="card__link card_in_process" href={proyect.url} target="_blank">
+                        <div className="card">
+                            <h2>{proyect.title} <small>( en desarrollo )</small></h2>
+                            <p className="card__title card_in_process">{proyect.description}</p>
                             <p className="card__apply">{proyect.language}<i className="fas fa-arrow-right"></i>
                             </p>
                         </div>
