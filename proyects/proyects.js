a6b88000ef2d2d8bea22c7ed9c882be72ae3ded5
Miguel Angel Caro <miguelangel.caro@iesdonana.org>
2023-04-17 15:12:58 +0200

    Updates

--- proyects/proyects.js
+++ proyects/proyects.js
@@ -1,20 +1,31 @@
 export const proyectos = [
     {
-        "url": "https://orienta-t.herokuapp.com/",
-        "title": "orientaT",
-        "description": "proyecto final grado superior desarrollo de aplicaciones web.",
-        "language": "HTML, CSS, JavaScript, PHP, Yii2, Ajax, JQuery", 
-    },
-    {
         "url": "https://miguetto.github.io/gif-explorer-app/",
         "title": "Gif Explorer",
         "description": "Buscador de gifs.",
-        "language": "HTML, CSS, JavaScript, React"
+        "language": "HTML, CSS, JavaScript, React",
+        "finalizado": true,
     },
     {
         "url": "https://miguetto.github.io/todo-app/",
         "title": "Todo App",
         "description": "Lista de tareas.",
-        "language": "HTML, CSS, JavaScript, React"
-    }
+        "language": "HTML, CSS, JavaScript, React",
+        "finalizado": true,
+    },
+    {
+        "url": "a",
+        "title": "Pokemon app",
+        "description": "Buscador de pokémon.",
+        "language": "HTML, CSS, JavaScript, React",
+        "finalizado": false,
+    },
+    {
+        "url": "https://orienta-t.herokuapp.com/",
+        "title": "orientaT",
+        "description": "proyecto final grado superior desarrollo de aplicaciones web.",
+        "language": "HTML, CSS, JavaScript, PHP, Yii2, Ajax, JQuery",
+        "finalizado": false,
+    },
+    
 ]
