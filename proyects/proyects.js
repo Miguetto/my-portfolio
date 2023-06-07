51a452796b84fc51dbc2f04fd61711bb63b35ccd
Miguel Angel Caro <migueliscb@gmail.com>
2023-06-07 14:24:16 +0200

    Updates

--- proyects/proyects.js
+++ proyects/proyects.js
@@ -1,5 +1,19 @@
 export const proyectos = [
     {
+        "url": "https://golden-moonbeam-88b4fb.netlify.app/characters",
+        "title": "StarWeb Personajes",
+        "description": "Web donde vemos los personajes de Star Wars con sus características y podemos añadir a favoritos los que más nos gusten.",
+        "language": "HTML, CSS, Tailwind, JavaScript, React, Hooks, Axios",
+        "finalizado": true,
+    },
+    {
+        "url": "https://comforting-boba-f56a18.netlify.app/",
+        "title": "Pokemon app",
+        "description": "Buscador de pokémon.",
+        "language": "HTML, CSS, JavaScript, React, Redux, Axios",
+        "finalizado": true,
+    },
+    {
         "url": "https://miguetto.github.io/gif-explorer-app/",
         "title": "Gif Explorer",
         "description": "Buscador de gifs.",
@@ -12,14 +26,7 @@ export const proyectos = [
         "description": "Lista de tareas.",
         "language": "HTML, CSS, JavaScript, React",
         "finalizado": true,
-    },
-    {
-        "url": "https://comforting-boba-f56a18.netlify.app/",
-        "title": "Pokemon app",
-        "description": "Buscador de pokémon.",
-        "language": "HTML, CSS, JavaScript, React, Redux",
-        "finalizado": true,
-    },
+    },    
     {
         "url": "https://orienta-t.herokuapp.com/",
         "title": "orientaT",
